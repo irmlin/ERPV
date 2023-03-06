1bbb5ceee1bbdd4714025230f05089928b697b26
Povilas Tamulis <povilas.tamulis@grainger.com>
2023-03-06 19:36:04 +0200

    ERPV-26 fixed migrations

--- backend/src/main/resources/db/migration/V1_3__roles_migration.sql
+++
@@ -1,7 +0,0 @@
-CREATE TYPE role_names AS ENUM ('ROLE_USER', 'ROLE_ADMIN')
-
-CREATE TABLE IF NOT EXISTS roles (
-    id UUID,
-    name role_names
-    PRIMARY KEY (id)
-);
---
+++ backend/src/main/resources/db/migration/V1_4__roles_migration.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS roles (
+     id UUID,
+     name role_names,
+     PRIMARY KEY (id)
+);
