635673399cff5f0be49d4b9c6ba72305c79ebafd
AurimasB <77635359+AurimasB@users.noreply.github.com>
2023-04-28 10:39:01 +0300

    Updated design before demo

--- frontend/Pages/AvatarPage.js
+++ frontend/Pages/AvatarPage.js
@@ -1,45 +1,57 @@
 import { useState } from "react";
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
-import Swiper from 'react-native-swiper';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
+import Swiper from "react-native-swiper";
 
-export default function AvatarPage({navigation}) {
-
+export default function AvatarPage({ navigation }) {
   const images = [
-    require('../assets/1.png'),
-    require('../assets/2.png'),
-    require('../assets/3.png'),
+    require("../assets/1.png"),
+    require("../assets/2.png"),
+    require("../assets/3.png"),
   ];
 
-  const [currentImageIndex, setCurrentImageIndex] = useState(0)
-  
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
   const onSwipeImageChange = (index) => {
-    setCurrentImageIndex(index)
-  }
+    setCurrentImageIndex(index);
+  };
 
   return (
-    <View style={styles.container}>
-      <Swiper
-        showsButtons={true}
-        loop={false}
-        index={currentImageIndex}
-        onIndexChanged={(index) => onSwipeImageChange(index)}
-        showsPagination={false}
-      >
-        {images.map((image, index) => (
-          <View key={index} style={styles.slide}>
-            <Image source={image} style={styles.image} />
-          </View>
-        ))}
-      </Swiper>
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Atrakinti</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Naudoti</Text>
-        </TouchableOpacity>
+    <ImageBackground
+      source={require("frontend/assets/backgrounds/avatarsBackground.png")}
+      resizeMode="cover"
+      style={{ width: "100%", height: "100%" }}
+    >
+      <View style={styles.container}>
+        <Swiper
+          showsButtons={true}
+          loop={false}
+          index={currentImageIndex}
+          onIndexChanged={(index) => onSwipeImageChange(index)}
+          showsPagination={false}
+        >
+          {images.map((image, index) => (
+            <View key={index} style={styles.slide}>
+              <Image source={image} style={styles.image} />
+            </View>
+          ))}
+        </Swiper>
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Atrakinti</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Naudoti</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
@@ -49,34 +61,34 @@ const styles = StyleSheet.create({
   },
   slide: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
     width: 300,
     height: 300,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
   buttonsContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 20,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    backgroundColor: '#6DD8E7',
+    backgroundColor: "#6DD8E7",
     borderRadius: 20,
     paddingVertical: 10,
     paddingHorizontal: 10,
     marginHorizontal: 5,
     marginVertical: 5,
-    alignItems: 'center',
+    alignItems: "center",
     width: "50%",
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 22,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
 });
