97f2e45d915084a0b42f8ed3ef1ab1e86b5d2594
irmlin <irmantas.l75@gmail.com>
2023-05-10 14:27:27 +0300

    removed different music from quiz page

--- frontend/Pages/QuizPage.js
+++ frontend/Pages/QuizPage.js
@@ -5,28 +5,27 @@ import { BackgroundMusicContext } from "../Contexts/BackgroundMusicContext";
 
 export default function QuizPage({navigation}) {
 
-  const { backgroundMusic, toggleMainSong, setToggleMainSong } = useContext(BackgroundMusicContext);
-
-  useEffect(() => {
-    async function playBackgroundMusic() {
-      try {
-        await backgroundMusic.unloadAsync();
-        await backgroundMusic.loadAsync(
-          require("frontend/assets/music/funky.mp3")
-        );
-        await backgroundMusic.playAsync();
-        backgroundMusic.setIsLoopingAsync(true);
-      } catch (error) {
-        console.log("Error playing background music:", error);
-      }
-    }
-    playBackgroundMusic();
-    return async () => {
-      await backgroundMusic.unloadAsync();
-      // this is necessary to change music back to main song
-      setToggleMainSong(!toggleMainSong);
-    };
-  }, []);
+  // const { backgroundMusic, toggleMainSong, setToggleMainSong } = useContext(BackgroundMusicContext);
+  // useEffect(() => {
+  //   async function playBackgroundMusic() {
+  //     try {
+  //       await backgroundMusic.unloadAsync();
+  //       await backgroundMusic.loadAsync(
+  //         require("frontend/assets/music/funky.mp3")
+  //       );
+  //       await backgroundMusic.playAsync();
+  //       backgroundMusic.setIsLoopingAsync(true);
+  //     } catch (error) {
+  //       console.log("Error playing background music:", error);
+  //     }
+  //   }
+  //   playBackgroundMusic();
+  //   return async () => {
+  //     await backgroundMusic.unloadAsync();
+  //     // this is necessary to change music back to main song
+  //     setToggleMainSong(!toggleMainSong);
+  //   };
+  // }, []);
 
   return (
     <View>
