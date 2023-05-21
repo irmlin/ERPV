35e5fb1e79c202d4e5fd0fed008b0c05981116e3
irmlin <irmantas.l75@gmail.com>
2023-05-21 18:14:51 +0300

    updated http client to access scanner via cloud

--- frontend/httpClient.js
+++ frontend/httpClient.js
@@ -2,15 +2,16 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL:
-        "http://192.168.0.116:8080",
+        "http://192.168.1.83:8080",
     headers: {
         "Content-Type": "application/json",
     },
 });
 
+// SCANNER HOSTED ON CLOUD, NO NEED TO CHANGE THE LINK
 export const axiosInstanceScanner = axios.create({
     baseURL:
-        "http://192.168.0.116:8000",
+        "https://pvp-scanner.azurewebsites.net/",
     headers: {
         "Content-Type": "application/json",
     },
